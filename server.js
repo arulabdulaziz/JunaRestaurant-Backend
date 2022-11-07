dba5062438e0db72ed6966a2bd861764019a84d6
arulabdulaziz <rulz.matrixs@gmail.com>
2022-11-07 19:33:33 +0700

    test

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const app = require("./app");
-// const http = require("http");
+// const http = require("http");/
 const port = process.env.PORT || 3000;
 
-// const server = http.createServer(app, () => {
-//   console.log("cekcek")
-// });
+const server = http.createServer(app, () => {
+  console.log("cekcek")
+});
 
 server.listen(port, () => {
   console.log(`listening web in http://localhost:${port}`);
