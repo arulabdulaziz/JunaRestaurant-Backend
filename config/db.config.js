e073096ac981e062169983d201be27632d5de565
arulabdulaziz <rulz.matrixs@gmail.com>
2022-11-07 17:53:01 +0700

    setup mongodb atlas

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,4 @@
-const collection = "juna_restaurant"
+const collection = process.env.MONGODB_COllECTION || "juna_restaurant";
 module.exports = {
   url: process.env.MONGODB_URI || "mongodb://localhost:27017/" + collection,
 };
